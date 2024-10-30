13f4477518bbc1db40a60568ad7ae763f7809ffe
17-Ahsan <143657641+17-Ahsan@users.noreply.github.com>
2024-10-30 01:21:26 -0700

    LandingPage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
+import LandingPage from "./pages/LandingPage";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/landingPage" element={<LandingPage />} />
+
       </Routes>
     </Router>
   );
